7e5e54dd4f080dad30615b5dea931ae3c75604b4
KamvelihleAthabileDyantyi17 <kamvelihleathabiledyantyi@gmail.com>
2026-03-04 09:26:01 +0200

    Add thank you page and bubble sort input form

---
+++ bubbleSort.php
@@ -0,0 +1,30 @@
+<html lang="en">
+<head>
+<body>
+<form method="post" action="process.php">
+<table>
+    <tr>
+        <td><input type="text" id="value_1" name="value_1"> </td>
+    </tr>
+    <tr>
+        <td><input type="text" id="value_2" name="value_2"> </td>
+    </tr>
+    <tr>
+        <td><input type="text" id="value_3" name="value_3"> </td>
+    </tr>
+    <tr>
+        <td><input type="text" id="value_4" name="value_4"> </td>
+    </tr>
+    <tr>
+        <td><input type="text" id="value_5" name="value_5"> </td>
+    </tr>
+    <tr>
+        <td><input type="submit" value="Submit"> </td>
+    </tr>
+</table>
+</form>
+<?php
+
+?>
+</body>
+</html>
