ee3e8cc89ead0ff9e645e1678d0f8fecff1d622e
KamvelihleAthabileDyantyi17 <kamvelihleathabiledyantyi@gmail.com>
2026-03-04 09:21:34 +0200

    Update database connection details for XAMPP configuration

--- process.php
+++ process.php
@@ -1,12 +1,11 @@
 <?php
+// 1. Database connection details (Updated for XAMPP defaults and your new DB)
+$servername = "localhost";
+$username = "root"; // Default XAMPP username is usually 'root'
+$password = "";     // Default XAMPP password is usually empty
+$dbname = "cars";   // FIX: Changed from contact_form_db to cars
 
-//Database connection parameters
-$servername = "localhost"; // Change if your database is hosted elsewhere
-$username = "kamva"; // Default XAMPP username
-$password = ""; // Default XAMPP password is empty
-$dbname = "contact_form_db"; // The name of your database
-
-//create connection to the database
+// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
@@ -14,11 +13,24 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-//collect form data using the $_POST superglobal array
-$firstName = $_POST['first_name'];
-$surname   = $_POST['surname'];
-$email     = $_POST['email'];
-$message = $_POST['message];'];
+// 2. Collect form data (These match the 'name' attributes in your HTML form)
+$type = $_POST['type'];
+$rim = $_POST['rim'];
+$horsepower = $_POST['horsepower'];
+$topspeed = $_POST['topspeed'];
 
-// This file will handle the form submission and process the data
+// 3. Prepare SQL query 
+// Notice the backticks ` ` around column names with spaces!
+$sql = "INSERT INTO brand (Type, Rim, `Horse Power`, `Top Speed`) 
+        VALUES ('$type', '$rim', '$horsepower', '$topspeed')";
+
+// 4. Execute SQL query
+if ($conn->query($sql) === TRUE) {
+    echo "New car added to the database successfully!";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+// Close connection
+$conn->close();
 ?>
